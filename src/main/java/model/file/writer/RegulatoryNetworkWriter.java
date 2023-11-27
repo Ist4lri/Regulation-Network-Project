4ef42ac1cace9e142e7f15d43589b5158c898d0d
Ist4lri <pierreloic.gouttebel@gmail.com>
2023-11-27 15:37:47 +0100

    Writer updated with simulationEvents

--- src/main/java/model/file/writer/RegulatoryNetworkWriter.java
+++ src/main/java/model/file/writer/RegulatoryNetworkWriter.java
@@ -3,11 +3,13 @@ package model.file.writer;
 import java.io.BufferedWriter;
 import java.io.IOException;
 
+import model.events.SimulationEvent;
 import model.genes.Gene;
 import model.network.RegulatoryNetwork;
 
 public class RegulatoryNetworkWriter {
     private GeneVisitor geneVisitor;
+    private EventVisitor eventVisitor;
 
     public RegulatoryNetworkWriter() {
     };
@@ -15,6 +17,7 @@ public class RegulatoryNetworkWriter {
     public void write(BufferedWriter bufferedWriter, RegulatoryNetwork regulatoryNetwork) throws IOException {
         this.writeConfiguration(bufferedWriter, regulatoryNetwork);
         this.writeGenes(bufferedWriter, regulatoryNetwork);
+        this.writeEvents(bufferedWriter, regulatoryNetwork);
     }
 
     private void writeGenes(BufferedWriter bufferedWriter, RegulatoryNetwork regulatoryNetwork) throws IOException {
@@ -27,6 +30,13 @@ public class RegulatoryNetworkWriter {
             throws IOException {
         bufferedWriter.write("TimeStep" + regulatoryNetwork.getTimeStepLength());
         bufferedWriter.write("TimeUpperBound" + regulatoryNetwork.getTimeUpperBound());
+    }
+
+    private void writeEvents(BufferedWriter bufferedWriter, RegulatoryNetwork regulatoryNetwork)
+            throws IOException {
+        for (SimulationEvent event : regulatoryNetwork.getSimulationEvents()) {
+            bufferedWriter.write(event.accept(eventVisitor));
+        }
     }
 
 }
