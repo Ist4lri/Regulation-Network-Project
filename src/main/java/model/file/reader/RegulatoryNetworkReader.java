e7c82ba16734bd3d435a5fd1c8639cdc1fc47fe4
Ist4lri <pierreloic.gouttebel@gmail.com>
2023-11-27 13:36:06 +0100

    Finishing Regu Network Reader

--- src/main/java/model/file/reader/RegulatoryNetworkReader.java
+++ src/main/java/model/file/reader/RegulatoryNetworkReader.java
@@ -8,6 +8,8 @@ import java.util.Map;
 
 import model.genes.Gene;
 import model.network.RegulatoryNetwork;
+import model.file.serializers.gene.ConcreteGeneSerializer;
+import model.file.serializers.gene.ConstantGeneSerializer;
 import model.file.serializers.gene.EntitySerializer;
 
 public class RegulatoryNetworkReader {
@@ -16,6 +18,9 @@ public class RegulatoryNetworkReader {
 
     public RegulatoryNetworkReader() {
         this.geneSerializers = new HashMap<>();
+        this.addGeneSerializer(ConcreteGeneSerializer.getInstance());
+        this.addGeneSerializer(ConstantGeneSerializer.getInstance());
+        this.genes = new HashMap<>();
     };
 
     private void addGeneSerializer(EntitySerializer<? extends Gene> serializer) {
@@ -35,18 +40,27 @@ public class RegulatoryNetworkReader {
     }
 
     public RegulatoryNetwork read(BufferedReader bufferedReader) throws IOException {
-        this.genes = new HashMap<>();
+        double timeStep = 0;
+        double timeUpperBound = 0;
+        int lineCounter = 0;
         for (String line = ""; line != null; line = bufferedReader.readLine()) {
             String[] dispatchElement = line.split(" ");
             switch (dispatchElement[0]) {
-                case "TimeStep", "TimeUpperBound":
+                case "TimeStep":
+                    timeStep = Double.parseDouble(dispatchElement[1]);
+                    break;
+                case "TimeUpperBound":
+                    timeUpperBound = Double.parseDouble(dispatchElement[1]);
                     break;
                 default:
                     if (this.geneSerializers.containsKey(dispatchElement[0])) {
-                        this.addGene(getGeneSerializer(dispatchElement[0]).deserialize(line, null));
+                        this.addGene(getGeneSerializer(dispatchElement[0]).deserialize(line, this));
+                    } else {
+                        throw new IOException("Error : Declare new geneSerializers type at this line :" + lineCounter);
                     }
             }
+            lineCounter++;
         }
-        return new RegulatoryNetwork(new ArrayList<>(this.genes.values()), null, 0, 0);
+        return new RegulatoryNetwork(new ArrayList<>(this.genes.values()), new ArrayList<>(), timeStep, timeUpperBound);
     }
 }
